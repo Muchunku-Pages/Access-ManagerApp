b6fcbd53f414a3985848cc752f8f736b8a7912a4
Brian Muchunku <brauxer@gmail.com>
2022-01-25 05:01:02 +0300

    Generate the Credentials object defining the key properties and functions of its instances

--- credentials.py
+++ credentials.py
@@ -1,4 +1,40 @@
 import random
 import string
 class Credentials:
+  """
+  The Class from which new Credential object instances
+  are to be generated.
+
+  """
+
+    credentials_object_list = [] #An Empty list to hold credential objects
+    
+
+    def __init__(self, account, username, password):
+      """
+       Method that generates the account credentials object to hold 
+       the details of each user instance
+
+      """
+
+      self.account = account
+      self.username = username
+      self.password = password
+
+    def save_credentials(self):
+      '''
+      Saves a credentials account instance to our application
+
+      '''
+      Credentials.credentials_object_list.append(self)
+
+
+    def delete_credentials(self):
+      '''
+      Deletes a saved account credentials instance from the 
+      credentials object list
+      
+      '''
+
+      Credentials.credentials_object_list.remove(self)
 
