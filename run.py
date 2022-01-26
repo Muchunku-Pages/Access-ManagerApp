95db528ea0869fd5dea48c279854765dba3c8998
Brian Muchunku <brauxer@gmail.com>
2022-01-26 03:22:19 +0300

    Complete formulating all the Credentials class methods to be executed by run.py and create a new unit test file for testing our run.py file executable code and avail the getpass and string classes by importing the native getpass and string python modules we require as we begin formulating the Code Logic Procedure for our run.py executable

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+import getpass
+import string
 from user import User
 from credentials import Credentials
 
+"""
+Begin by formulating the User class dependent 
+executable run functions 
+
+"""
 def create_user(username, password):
   '''
   A function for generating a new user instance
@@ -53,22 +60,67 @@ def display_all_users():
   return User.user.display_all_users()
 
 
+"""
+Next formulate the Credential class dependent 
+executable run functions 
+
+"""
+
+def create_credentials(self, account, username, password):
+  '''
+  A function to generate new account credentials
+
+  '''
+
+  new_credentials = Credentials(account, username password)
+  return new_credentials
 
 
+def save_credentials(new_credentials):
+  '''
+  A function to save credentials by calling the 
+  Credentials class save_credentials method property
+
+  '''
+  Credentials.save_credentials()
 
 
+def delete_credentials(credentials):
+  '''
+  A function to remove an account credential instance by calling 
+  the Credentials class delete credentials method property
+
+  '''
+  Credentials.delete_credentials()
+
+def find_credentials(username):
+  '''
+  A Function which searches for an account credential 
+  by its username and returns the associated instance 
+  by calling the Credential class method find credentials
+
+  '''
+  return Credentials.find_credentials(username)
 
 
+def check_existing_credentials(username):
+  '''
+  A Function to check whether an account credential 
+  with the username argument value already exists 
+  and return a boolean result by calling the  
+  Credential class method property credential_confirmation
+
+  '''
+  return Credentials.credential_confirmation(username)
 
 
+def display_credentials();
+  '''
+  A Function which returns instances of all the 
+  saved account credentials by calling the 
+  Credential class method property display credentials
 
+  '''
 
-
-
-
-
-
-
-
-
+  return Credentials.display_credentials()
 
